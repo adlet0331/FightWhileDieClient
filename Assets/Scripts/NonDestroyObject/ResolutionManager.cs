5ae91839275e5b5d8ea58764391d7dcf84eeda29
Adlet <eypk777@gmail.com>
2022-11-15 02:00:16 +0900

    Resources 폴더에서 빌드때 포함 안되게 Resource 폴더로 옮김. Resolution 높이 제한 설정

--- Assets/Scripts/NonDestroyObject/ResolutionManager.cs
+++ Assets/Scripts/NonDestroyObject/ResolutionManager.cs
@@ -6,14 +6,15 @@ namespace NonDestroyObject
     public class ResolutionManager : Singleton<ResolutionManager>
     {
         [Header(("Size"))]
-        [SerializeField] private int FixedWidth;
+        [SerializeField] private float FixedWidth;
+        [SerializeField] private float HeightTopLimit;
         [Header("Components")]
         [SerializeField] private Camera MainCamera;
         [SerializeField] private Canvas MainCanvas;
         private void Start()
         {
             Debug.Log(MainCamera.aspect);
-            MainCamera.orthographicSize = FixedWidth / ((float)Screen.width / Screen.height);
+            MainCamera.orthographicSize = FixedWidth / ((float)Screen.width / Screen.height) < 8 ? FixedWidth / ((float)Screen.width / Screen.height) : 8;
         }
     }
 }
