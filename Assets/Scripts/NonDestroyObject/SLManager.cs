9cac20dcbd5219b88a1d1cbcfa844c71200e7bff
Adlet <eypk777@gmail.com>
2022-12-03 06:10:42 +0900

    Make up SLManager, Run SaveInServer in Task

--- Assets/Scripts/NonDestroyObject/SLManager.cs
+++ Assets/Scripts/NonDestroyObject/SLManager.cs
@@ -31,7 +31,69 @@
         private void Start()
         {
             LoadPrefs();
-            UpdateUI();
+            SaveAllInfos();
+        }
+
+        public void LoadPrefs()
+        {
+            _name = PlayerPrefs.GetString("Name", String.Empty);
+            _id = PlayerPrefs.GetInt("Id", -1);
+            _name = PlayerPrefs.GetString("Name", "");
+            _topStage = PlayerPrefs.GetInt("TopStage", 1);
+            _baseAtk = PlayerPrefs.GetInt("BaseAtk", 50);
+            _coin = PlayerPrefs.GetInt("Coin", 10);
+            _stage = ((int)(_topStage / 10.0f) * 10) + 1;
+            UpdateAllStatus();
+        }
+        
+        public void InitUser(int id, string userName)
+        {
+            _id = id;
+            _name = userName;
+            PlayerPrefs.SetInt("Id", id);
+            PlayerPrefs.SetString("Name", userName);
+            UIManager.Instance.UpdateUserName(_name);
+        }
+
+        private void UpdateAtk()
+        {
+            _atk = _baseAtk;
+            PlayerCombatManager.Instance.Player.UpdateStatus(1, _atk);
+        }
+
+        private void UpdateClearCoin()
+        {
+            _clearCoin = _stage;
+        }
+
+        private void UpdateEnemyHp()
+        {
+            _enemyHp = (int)(50 * Math.Pow(1.2f, _stage));
+            CombatManager.Instance.AI.UpdateStatus(_enemyHp, 1);
+        }
+
+        private void UpdateUI()
+        {
+            UIManager.Instance.UpdateUserName(_name);
+            UIManager.Instance.UpdateStage(_stage);
+            UIManager.Instance.UpdateEnemyHp(_enemyHp);
+            UIManager.Instance.UpdateAttackVal(_atk);
+            UIManager.Instance.UpdateCoinVal(_coin);
+        }
+        
+        private void SavePrefs()
+        {
+            if (_stage > _topStage)
+            {
+                _topStage = _stage;
+                PlayerPrefs.SetInt("TopStage", _topStage);
+            }
+            PlayerPrefs.SetInt("BaseAtk", _baseAtk);
+            PlayerPrefs.SetInt("Coin", _coin);
+        }
+        
+        private void SaveAllInfos()
+        {
             try
             {
                 var checkConnection = Task.Run(() => NetworkManager.Instance.CheckConnection());
@@ -71,75 +133,24 @@
                 Debug.Log(e);
             }
         }
-        
-        public void SavePrefs()
-        {
-            if (_stage > _topStage)
-            {
-                _topStage = _stage;
-                PlayerPrefs.SetInt("TopStage", _topStage);
-            }
-            PlayerPrefs.SetInt("BaseAtk", _baseAtk);
-            PlayerPrefs.SetInt("Coin", _coin);
-        }
 
-        public void LoadPrefs()
+        private void UpdateAllStatus()
         {
-            _name = PlayerPrefs.GetString("Name", String.Empty);
-            _id = PlayerPrefs.GetInt("Id", -1);
-            _name = PlayerPrefs.GetString("Name", "");
-            _topStage = PlayerPrefs.GetInt("TopStage", 1);
-            _baseAtk = PlayerPrefs.GetInt("BaseAtk", 50);
-            _coin = PlayerPrefs.GetInt("Coin", 10);
-            _stage = ((int)(_topStage / 10.0f) * 10) + 1;
+            // Update variables
             UpdateAtk();
             UpdateEnemyHp();
             UpdateClearCoin();
-        }
-        
-        public void InitUser(int id, string userName)
-        {
-            _id = id;
-            _name = userName;
-            PlayerPrefs.SetInt("Id", id);
-            PlayerPrefs.SetString("Name", userName);
-            UIManager.Instance.UpdateUserName(_name);
-        }
-
-        public void UpdateAtk()
-        {
-            _atk = _baseAtk;
-            PlayerCombatManager.Instance.Player.UpdateStatus(1, _atk);
-        }
-
-        public void UpdateClearCoin()
-        {
-            _clearCoin = _stage;
-        }
-
-        public void UpdateEnemyHp()
-        {
-            _enemyHp = (int)(50 * Math.Pow(1.2f, _stage));
-            CombatManager.Instance.AI.UpdateStatus(_enemyHp, 1);
-        }
-
-        private void UpdateUI()
-        {
-            UIManager.Instance.UpdateUserName(_name);
-            UIManager.Instance.UpdateStage(_stage);
-            UIManager.Instance.UpdateEnemyHp(_enemyHp);
-            UIManager.Instance.UpdateAttackVal(_atk);
-            UIManager.Instance.UpdateCoinVal(_coin);
+            // Update UI
+            UpdateUI();
+            // Update Prefs and Server
+            SavePrefs();
+            Task.Run(() => SaveAllInfos());
         }
 
         public void StageReset()
         {
             _stage = ((int)(_topStage / 10.0f) * 10) + 1;
-            UpdateAtk();
-            UpdateEnemyHp();
-            UpdateClearCoin();
-            UpdateUI();
-            SavePrefs();
+            UpdateAllStatus();
         }
         
         public void StageCleared()
@@ -147,11 +158,7 @@
             _stage += 1;
             _baseAtk += 10;
             _coin += _stage;
-            UpdateAtk();
-            UpdateEnemyHp();
-            UpdateClearCoin();
-            UpdateUI();
-            SavePrefs();
+            UpdateAllStatus();
         }
     }
 }
