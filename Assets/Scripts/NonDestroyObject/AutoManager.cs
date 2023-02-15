6f317e431168fc19d350633921333e8d6e0faed1
AdletMier <eypk777@gmail.com>
2023-02-16 02:08:13 +0900

    Add Auto Button

---
+++ Assets/Scripts/NonDestroyObject/AutoManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace NonDestroyObject
+{
+    public delegate void AutoChanged();
+    public class AutoManager : Singleton<AutoManager>
+    {
+        [Header("Status")]
+        [SerializeField] private bool isAuto;
+
+        public bool IsAuto
+        {
+            get => isAuto;
+            set
+            {
+                isAuto = value;
+                AutoChanged?.Invoke();
+            }
+        }
+        public event AutoChanged AutoChanged;
+        
+    }
+}
---
+++ Assets/Scripts/UI/AutoButton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NonDestroyObject;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class AutoButton : MonoBehaviour
+    {
+        [SerializeField] private Image background;
+        [SerializeField] private Image autoText;
+        [SerializeField] private Image border;
+        [SerializeField] private bool isAuto;
+
+        private void Start()
+        {
+            isAuto = false;
+            AutoManager.Instance.AutoChanged += UpdateUI;
+        }
+
+        public void AutoButtonClicked()
+        {
+            AutoManager.Instance.IsAuto = !AutoManager.Instance.IsAuto;
+        }
+
+        public void UpdateUI()
+        {
+            isAuto = AutoManager.Instance.IsAuto;
+                    
+            if (isAuto)
+            {
+                background.color = new Color(1.0f, 1.0f,0.0f,1.0f);
+                autoText.color = new Color(0.1f, 0.1f,0.1f,1.0f);
+                border.color = new Color(1.0f, 0.0f,0.0f,1.0f);
+            }
+            else
+            {
+                background.color = new Color(1.0f, 1.0f,1.0f,1.0f);
+                autoText.color = new Color(0.0f, 0.0f,0.0f,1.0f);
+                border.color = new Color(0.0f, 0.0f,0.0f,1.0f);
+            }
+        }
+    }
+}
