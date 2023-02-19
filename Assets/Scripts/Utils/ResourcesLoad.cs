191e13dc9bf72d9cd47b28887b5997c236ac8af1
AdletMier <eypk777@gmail.com>
2023-02-20 02:40:18 +0900

    Add Sprites For All Option

--- Assets/Scripts/Utils/ResourcesLoad.cs
+++ Assets/Scripts/Utils/ResourcesLoad.cs
@@ -8,8 +8,6 @@ namespace Utils
         public static Sprite LoadEquipmentSprite(int rare, int option)
         {
             var spriteName = DataManager.Instance.staticDataManager.GetEquipItemInfo(rare, option).spriteName;
-            Debug.Log(spriteName);
-            spriteName = "01";
             return Resources.Load<Sprite>("Sprites\\ItemEquipment\\" + spriteName);
         }
     }
